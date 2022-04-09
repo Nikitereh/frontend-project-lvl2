9f0c3848ef986c7510e2e629f2762f770272b918
Nikitereh <nikita607693@yandex.ru>
2022-04-09 18:47:18 +0300

    add options and args

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
 
 program.parse();
 
