53f62b1591adaf1bc465406b0acc3be39ead4dd1
Nikitereh <nikita607693@yandex.ru>
2022-04-09 16:57:37 +0300

    add node_modules

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
-  .Options('-h, --help', 'output usage information')
-
+  .helpOption('-h, --help', 'output usage information')
+  console.log();
 program.parse();
 
