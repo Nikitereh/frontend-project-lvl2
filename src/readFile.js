a2301092aebb60310c3541c7390d2869fe53b115
Nikitereh <99133872+Nikitereh@users.noreply.github.com>
2022-04-14 10:44:45 +0300

    fix

--- src/readFile.js
+++ src/readFile.js
@@ -1,11 +1,22 @@
-import { resolve } from 'path';
-import { readFileSync } from 'fs';
+import readFile from './readFile.js';
+import parse from './parse.js';
+import genTree from './genTree.js';
+import formattedTree from './formatters/index.js';
 
-const getFixturePath = (filename) => {
-  return resolve(process.cwd(), filename) 
-};
-const readFile = (filename) => {
-  return readFileSync(getFixturePath(filename), 'utf-8') // 
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const fileContent1 = readFile(filepath1);
+  console.log('fileContent1',fileContent1);
+  const fileContent2 = readFile(filepath2);
+  console.log('fileContent2',fileContent2);
+
+  const parsedFile1 = parse(fileContent1, filepath1.split('.').at(-1));
+  console.log('parsedFile1',parsedFile1)
+  const parsedFile2 = parse(fileContent2, filepath2.split('.').at(-1));
+  console.log('parsedFile2',parsedFile2)
+
+  const tree = genTree(parsedFile1, parsedFile2);
+
+  return formattedTree(tree, formatName);
 };
 
-export default readFile;
+export default genDiff;
