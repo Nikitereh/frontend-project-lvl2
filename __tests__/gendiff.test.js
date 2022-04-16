810cfb6a81c5501dba3cee08a5a3382c4f51e453
Nikitereh <99133872+Nikitereh@users.noreply.github.com>
2022-04-16 10:30:42 +0300

    add test

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import genDiff from '../src';
+
+const extensionsWithFormats = [
+  ['json', 'nested'], ['json', 'plain'], ['json', 'json'],
+  ['yml', 'nested'], ['yml', 'plain'], ['yml', 'json'],
+  ['ini', 'nested'], ['ini', 'plain'], ['ini', 'json'],
+];
+const fixturesPath = `${__dirname}/__fixtures__/`;
+const getExpectedResult = (format) => (fs.readFileSync(`${__dirname}/__fixtures__/${format}Result.txt`, 'utf8'));
+
+test.each(extensionsWithFormats)(
+  'genDiff(%s), format=%s',
+  (ext, format) => {
+    const result = genDiff(`${fixturesPath}before.${ext}`, `${fixturesPath}after.${ext}`, format);
+    expect(result).toMatch(getExpectedResult(format));
+  },
+);
+
+test('genDiff(empty)', () => {
+  expect(() => genDiff()).toThrow();
+});
