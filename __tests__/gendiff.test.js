3ff449c7cc7826d65f323678541d0e88b240f4b7
Nikitereh <99133872+Nikitereh@users.noreply.github.com>
2022-04-16 18:33:55 +0300

    add lint and babel

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,20 +2,11 @@ import fs from 'fs';
 import genDiff from '../src';
 
 const extensionsWithFormats = [
-  ['json', 'nested'], ['json', 'plain'], ['json', 'json'],
-  ['yml', 'nested'], ['yml', 'plain'], ['yml', 'json'],
-  ['ini', 'nested'], ['ini', 'plain'], ['ini', 'json'],
+  ['json'],
 ];
 const fixturesPath = `${__dirname}/__fixtures__/`;
 const getExpectedResult = (format) => (fs.readFileSync(`${__dirname}/__fixtures__/${format}Result.txt`, 'utf8'));
 
-test.each(extensionsWithFormats)(
-  'genDiff(%s), format=%s',
-  (ext, format) => {
-    const result = genDiff(`${fixturesPath}before.${ext}`, `${fixturesPath}after.${ext}`, format);
-    expect(result).toMatch(getExpectedResult(format));
-  },
-);
 
 test('genDiff(empty)', () => {
   expect(() => genDiff()).toThrow();
