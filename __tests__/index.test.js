5af0cddecdfadd30a583c93dc99059f647c9a470
Nikitereh <99133872+Nikitereh@users.noreply.github.com>
2022-04-23 11:39:12 +0300

    test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,36 +16,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const tests = [
-  {
-    filename1: 'file1.json', filename2: 'file2.json', output: 'expected-stylish.txt', formatName: 'stylish',
-  },
-  {
-    filename1: 'file1.yml', filename2: 'file2.yaml', output: 'expected-stylish.txt', formatName: 'stylish',
-  },
-  {
-    filename1: 'file1.json', filename2: 'file2.json', output: 'expected-plain.txt', formatName: 'plain',
-  },
-  {
-    filename1: 'file1.yml', filename2: 'file2.yaml', output: 'expected-json.txt', formatName: 'plain',
-  },
-  {
-    filename1: 'file1.json', filename2: 'file2.json', output: 'expected-json.txt', formatName: 'json',
-  },
-  {
-    filename1: 'file1.yml', filename2: 'file2.yaml', output: 'expected-json.txt', formatName: 'json',
-  },
-];
 
-test.each(tests)('gendiff stylish, plain and json tests', ({
-  filename1, filename2, output, formatName,
-}) => {
-  const filepath1 = getFixturePath(filename1);
-  const filepath2 = getFixturePath(filename2);
-  const expected = readFile(output);
-  const result = genDiff(filepath1, filepath2, formatName);
-  expect(result).toEqual(expected);
-});
 
 test('genDiff(empty)', () => {
   expect(() => genDiff()).toThrow();
